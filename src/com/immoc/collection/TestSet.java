5404323d74e220fce78a550f6036149916d4137e
446816 <446816997@qq.com>
2018-12-21 21:53:00 +0800

    Set接口

---
+++ src/com/immoc/collection/TestSet.java
@@ -0,0 +1,17 @@
+package com.immoc.collection;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class TestSet {
+    public List<Course> courseCourseSet;
+
+    public void TestSet(){
+        courseCourseSet = new ArrayList<>();
+        Course [] cr = new Course[]{"1","C语言"),();
+        courseCourseSet.add(cr);
+
+    }
+}
